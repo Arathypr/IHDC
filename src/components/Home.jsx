45d0b3feb3c3c7550434926a49d6efb2b38806ac
Arathy <arathyp.radhakrishnan@gmail.com>
2024-02-14 03:36:51 +0530

    completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,102 +1,3 @@
-// import React, { useState } from "react";
-// import logo from "../assets/logo.png";
-
-// const Home = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   return (
-//     <>
-//       <header className="bg-slate-200 sticky top-0 z-[20] mx-auto flex w-full items-center justify-around">
-//         <div>
-//           <img src={logo} className="h-16 w-16 " alt="" />
-//         </div>
-//         <div>
-//           <h1>XYZ SYSTEMS LLP.</h1>
-//         </div>
-//         <div className="text-transparent">
-//           <h1>X</h1>
-//         </div>
-//         <div className="lg:hidden">
-//           <button onClick={toggleMenu} className="focus:outline-none">
-//             ☰
-//           </button>
-//         </div>
-//       </header>
-//       <nav
-//         className={`lg:flex lg:items-center lg:justify-around text-black text-center ${
-//           menuOpen ? "block" : "hidden"
-//         } lg:block py-2`}
-//       >
-//         <ul className="lg:flex space-x-4 font-semibold text-xs">
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Home
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Dash Board
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Products
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Transactions
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Journal
-//             </a>
-//           </li>
-//         </ul>
-//       </nav>
-//       <div
-//         className={`lg:hidden ${
-//           menuOpen ? "block" : "hidden"
-//         } absolute top-16 right-0 bg-slate-200 p-20 w-screen h-screen border border-gray-300`}
-//       >
-//         <ul className="space-y-8">
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Home
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Dash Board
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Products
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Transactions
-//             </a>
-//           </li>
-//           <li>
-//             <a href="#" className="hover:text-gray-300">
-//               Journal
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Home;
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
@@ -112,7 +13,7 @@ const Home = () => {
   return (
     <>
       <div className="relative">
-        <div className="bg-slate-200 sticky top-0 z-[20] mx-auto flex w-full items-center justify-around">
+        <div className="bg-slate-200 sticky top-0 z-[30] mx-auto flex w-full items-center justify-around">
           <div>
             <img src={logo} className="h-16 w-16 " alt="" />
           </div>
@@ -123,7 +24,7 @@ const Home = () => {
             <h1>X</h1>
           </div>
 
-          <div className="lg:hidden">
+          <div className="lg:hidden ">
             <button onClick={toggleMenu} className="focus:outline-none">
               ☰
             </button>
@@ -135,7 +36,7 @@ const Home = () => {
             menuOpen ? "block" : "hidden"
           } lg:block py-2`}
         >
-          <ul className="lg:flex space-x-4 font-semibold text-xs">
+          <ul className="lg:flex space-x-4 font-semibold text-base">
             <li>
               <a href="#" className="hover:text-gray-300">
                 Home
@@ -167,7 +68,8 @@ const Home = () => {
         <div
           className={`lg:hidden ${
             menuOpen ? "block" : "hidden"
-          } absolute top-16 right-0 bg-slate-200 p-20 w-screen h-screen border border-gray-300`}
+          } absolute top-16 right-0 bg-slate-200 p-20 w-full h-[130vh] border border-gray-300`}
+          style={{ zIndex: menuOpen ? 10 : 5 }}
         >
           <ul className="space-y-8">
             <li>
